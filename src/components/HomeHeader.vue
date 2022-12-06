f948e99b26d1753099a4c2c5847cbaf49db4d13b
Angang Evelyn <angang.evelyn@student.tdsb.on.ca>
2022-12-05 19:45:32 -0500

    added some CSS to SiteMovies.Vue

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -5,47 +5,47 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const button = () => {
-    router.push("/loginPage");
+	router.push("/loginPage");
 }
 </script>
 
 <template>
-    <div>
-        <p>Movie Company Name</p>
-        <button @click="button">Login</button>
-    </div>
+	<div>
+		<p>Movie Company Name</p>
+		<button @click="button">Login</button>
+	</div>
 </template>
 
 <style scoped>
 div {
-    background-color: white;
-    border-bottom: 5px;
-    border-style: solid;
-    border-color: rgba(1, 180, 228, 0.5);
-    z-index: 1;
+	background-color: white;
+	border-bottom: 5px;
+	border-style: solid;
+	border-color: rgba(1, 180, 228, 0.5);
+	z-index: 1;
 }
 
 p {
-    display: inline;
-    font-size: 50px;
-    padding-left: 10px;
-    font-weight: 500;
+	display: inline;
+	font-size: 50px;
+	padding-left: 10px;
+	font-weight: 500;
 }
 
 button {
-    width: 100px;
-    height: 40px;
-    font-size: 20px;
-    border-radius: 10px;
-    float: right;
-    margin-top: 20px;
-    background-color: rgba(137, 137, 137, 0.5);
-    margin-right: 10px;
+	width: 100px;
+	height: 40px;
+	font-size: 20px;
+	border-radius: 10px;
+	float: right;
+	margin-top: 20px;
+	background-color: rgba(137, 137, 137, 0.5);
+	margin-right: 10px;
 }
 
 button:hover {
-    background-color: rgba(1, 180, 228, 0.5);
-    color: rgb(255, 255, 255);
-    box-shadow: 1.5px 1.5px rgba(137, 137, 137, 0.8);
+	background-color: rgba(1, 180, 228, 0.5);
+	color: rgb(255, 255, 255);
+	box-shadow: 1.5px 1.5px rgba(137, 137, 137, 0.8);
 }
 </style>
--- src/components/SiteMovies.vue
+++ src/components/SiteMovies.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue';
 import axios from "axios";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const movie = ref(null);
 const movieName = ref("");
-const imageSrc = ref("");
+const input = ref("");
 
 const getData = async (url, params) => {
 	try {
@@ -25,21 +27,50 @@ const getPopular = async () => {
 	console.log(movie.value)
 	movieName.value = movie.value.results
 	console.log(movieName)
-	// for (let result in movieName) {
-	// 	imageSrc.value = "https://image.tmdb.org/t/p/w500" + result.poster_path
-	// }
 }
 getPopular()
+
+const info = async () => {
+	const data = input.value;
+	movie.value = (await getData(`https://api.themoviedb.org/3/movie/${data}`, {
+		params: {
+			api_key: "c38e6d2014c822c96f368ab7d8dd502d",
+			append_to_response: "videos",
+		}
+	})).data;
+	// for (result in movieName) {
+	// 	console.log(result.poster_path)
+	// }
+}
 </script>
 
 <template>
-	<div v-for="result in movieName">
-		<img :src='`https://image.tmdb.org/t/p/w500${result.poster_path}`' alt="">
+	<!-- Add something for top movie? -->
+	<div class="movie-container">
+		<div v-for="result in movieName">
+			<img :src='`https://image.tmdb.org/t/p/w500${result.poster_path}`' alt="" @click="info">
+		</div>
 	</div>
 </template>
 
 <style scoped>
-div {
-	color: black;
+.movie-container {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 11px;
+	align-items: center;
+	justify-content: space-evenly;
+	margin-top: 10px;
+}
+
+img {
+	width: 200px;
+	border-radius: 5px;
+}
+
+img:hover {
+	box-shadow: 0 0 8px 2px rgb(1, 180, 228);
+
 }
 </style>
